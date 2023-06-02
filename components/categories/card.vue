5c572f731e9c4b512f8b7f950ec6590c811c99b3
Namianya <wekesa350@gmail.com>
2023-06-02 05:03:42 +0300

    categories card

---
+++ components/categories/card.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-col gap-2 lg:w-28 w-20">
+
+        <div class="avatar">
+            <div class="w-20 lg:w-28 rounded">
+                <NuxtImg
+                    :src="img"
+                    class="w-20 lg:w-28"
+                />
+            </div>
+        </div>
+        <p class="text-center text-sm capitalize  ">{{ categoryTitle }}</p>
+
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+    img: {
+        type: String,
+        required: true
+    },
+    categoryTitle: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped></style>
