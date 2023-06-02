39a4bcf39a7bc211a3ffd8570392f12e32ef263a
Namianya <wekesa350@gmail.com>
2023-06-02 05:04:38 +0300

    navbar

---
+++ components/top/nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex justify-between">
+        <div>
+            <button>
+            </button>
+        </div>
+
+        <div></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
