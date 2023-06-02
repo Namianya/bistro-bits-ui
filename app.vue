84c37985970486c55620fcc6bd5ac1157b17cd6b
Namianya <wekesa350@gmail.com>
2023-06-02 04:59:37 +0300

    Added routes

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-blue-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
