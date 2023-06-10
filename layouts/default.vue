36385e6ef8af0a011c1abdff3aad79b055c49ae4
Namianya <wekesa350@gmail.com>
2023-06-10 14:31:01 +0300

    full width

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex h-screen">
-        <div class=" w-full lg:max-w-2xl  h-screen relative">
+        <div class=" w-full   h-[100dvh] relative">
 
             <div class="absolute top-0 left-0 w-full z-50">
                 <TopNav />
@@ -8,9 +8,7 @@
 
             <slot />
         </div>
-        <div class="hidden lg:flex flex-auto  h-screen  justify-center items-center">
-            My Cafe
-        </div>
+
     </div>
 </template>
 
