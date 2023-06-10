0b97eb9045a0af82643c3cf84b0660e3cbc3378d
Namianya <wekesa350@gmail.com>
2023-06-10 14:29:42 +0300

    dynamic data

--- components/food/menu.vue
+++ components/food/menu.vue
@@ -9,7 +9,8 @@
             </div>
             <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black"></div>
 
-            <section class="flex overflow-x-scroll lg:overflow-x-clip gap-4 scrollbar-hide pt-8 mb-6 px-4 ">
+            <section
+                class="flex overflow-x-scroll lg:overflow-x-clip gap-4 scrollbar-hide pt-8 mb-6 px-4 md:px-4 lg:px-16 ">
                 <div
                     class="cursor-pointer"
                     v-for="foodCartegory in foodCartegories"
@@ -22,19 +23,33 @@
                 </div>
             </section>
         </div>
-        <div class="m-4 lg:overflow-x-scroll lg:scrollbar-hide">
+        <div class="m-4 lg:overflow-x-scroll lg:scrollbar-hide md:px-4 lg:px-16">
             <p class="font-bold text-xl text-slate-700 dark:text-slate-200 mb-4">
                 Popular
             </p>
-            <div class="space-y-2">
+
+            <div
+                v-if="menuItems.length"
+                class="space-y-2"
+            >
                 <div
-                    v-for="foodItem in foodList"
-                    :key="foodItem.id"
+                    v-for="menuItem in menuItems"
+                    :key="menuItem.data.id"
                 >
-                    <NuxtLink :to="`/menu/${foodItem.id}-details`">
-                        <FoodMenuCard :food-item="foodItem" />
+                    <NuxtLink :to="`/menu/${menuItem.data.id}-details`">
+                        <FoodMenuCard :food-item="menuItem.data" />
                     </NuxtLink>
                 </div>
+            </div>
+            <div
+                v-else
+                class="flex h-full flex-col p-4 bg-white dark:bg-slate-900 rounded shadow-lg "
+            >
+                <Icon
+                    name="solar:oven-mitts-broken"
+                    class="text-red-500/50 w-12 h-12 mx-auto"
+                />
+                <p class="text-center">Menu Not Available</p>
             </div>
         </div>
     </div>
@@ -42,12 +57,30 @@
 
 <script setup lang="ts">
 import { foodList } from '~/data/food';
+import { APIFormart } from '~/types/foodType';
+import { API_URL } from '~/variables/urls';
+
 defineProps({
     foodCartegories: {
         type: Array<{ img: string, categoryTitle: string }>,
         required: true
+    },
+    menuItems: {
+        type: Array<APIFormart>,
+        required: true
+    },
+    loading: {
+        type: Boolean,
+        default: false
     }
 })
+
+
+
+
+
+
+
 </script>
 
 <style scoped></style>
