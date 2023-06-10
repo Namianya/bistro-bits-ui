a73c7fbed654cc1ea22295dd05856477fddc205d
Namianya <wekesa350@gmail.com>
2023-06-10 14:31:42 +0300

    tagline ang background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <template>
 	<div class="h-screen text-center bg-slate-100 relative">
 		<div
-			class="absolute top-0 left-0 h-screen w-full bg-[url('https://images.unsplash.com/photo-1540189549336-e6e99c3679fe')] object-fill bg-cover">
+			class="absolute top-0 left-0 h-screen w-full bg-[url('https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')] lg:bg-[url('https://images.unsplash.com/photo-1452967712862-0cca1839ff27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80')] object-fill bg-cover">
 		</div>
 
 		<div class="absolute top-0 left-0 h-screen w-full bg-gradient-to-b from-black to-black/30"></div>
 		<div class="relative top-0 left-0">
 			<div class="flex flex-col justify-between items-center h-screen">
-				<p class="font-medium text-2xl text-white mt-28 max-w-md">
-					Lorem ipsum dolor sit amet consectetur.
-				</p>
+
+				<div class="mt-28">
+					<p class="text-orange-500 font-bold text-2xl lg:text-3xl mb-4"><span
+							class="text-lime-500">Bistro</span>Bits
+					</p>
+					<p class="font-medium text-2xl text-white  max-w-md">
+						Creating Memories, One <span class="text-lime-500">Bite</span> at a <span
+							class="text-orange-500">Time</span>
+					</p>
+				</div>
 				<div class="flex justify-center gap-4">
 					<!-- <p class="text-orange-500">hdshjsd</p> -->
 					<NuxtLink
